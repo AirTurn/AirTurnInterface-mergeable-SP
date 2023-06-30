c221e1c8f7640a7a5ab278af35833184b112d643
Nick Brook <nrbrook@gmail.com>
2023-06-30 14:49:48 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let version = "4.7.1-b.1"
+let checksum = "8338733f6ec912658abeb4698e9b014373a5c3bab81a074c81cf6b1d5f8220ea"
+
+let package = Package(
+    name: "AirTurnInterface",
+    platforms: [.iOS(.v11)],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "AirTurnInterface",
+            targets: ["AirTurnInterface"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .binaryTarget(name: "AirTurnInterface", url: "https://dev.airturn.com/sdk/AirTurnInterface.\(version).mergeable.spm.zip", checksum: checksum)
+    ]
+)
